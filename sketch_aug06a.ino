369b7a757a8195f072a581d836e9de631f9e3d11
Tomas Härdin <tomas.hardin@codemill.se>
2014-09-23 23:43:33 +0200

    Calibrate DAC, add stupid decay code
    The DAC calibration works by measuring each DAC output voltage with ADC channel 0.
    The results are used to compute a LUT that fixes imperfections caused by poor DAC resistor accuracy.

--- sketch_aug06a.ino
+++ sketch_aug06a.ino
@@ -8,25 +8,36 @@ byte vol[128];
 #define SRATE 44100         //sample rate
 #define PINOFS 3
 #define MAX_KEYS 8
+#define DAC_BITS 6
+
 short osc[MAX_KEYS];
 unsigned char keys[MAX_KEYS];
 unsigned char vols[MAX_KEYS];
-unsigned char nkeys = 0;
+unsigned char keys2[MAX_KEYS];
+unsigned char vols2[MAX_KEYS];
+unsigned char newkeys;      //set when a new keys/vols buffer can be copied
+unsigned char lut[1 << DAC_BITS];	//DAC correction LUT
+
+#define DAC_IN A0			//pin to read DAC from
 
 void computeKeys() {
+  while (newkeys);
+
   for (unsigned char x = 0; x < MAX_KEYS; x++) {
-    keys[x] = 128;
-    vols[x] = 0;
+    keys2[x] = 128;
+    vols2[x] = 0;
   }
-  nkeys = 0;
+  unsigned char nkeys = 0;
   for (unsigned char x = 0; x < 128; x++) {
     if (vol[x]) {
-      vols[nkeys]   = vol[x];
-      keys[nkeys++] = x;
+      vols2[nkeys]   = vol[x];
+      keys2[nkeys++] = x;
       if (nkeys == MAX_KEYS)
         break;
     }
   }
+
+  newkeys = 1;
 }
 
 void setup() {
@@ -45,6 +56,7 @@ void setup() {
 
   DDRB = 1;
   DDRD = 0xFF;
+  newkeys = 0;
 
   for (char x = 0;; x++) {
     //A4 = 69 (440 Hz)
@@ -57,23 +69,52 @@ void setup() {
   }
   freq[128] = 0;
   computeKeys();
+
+  //calibrate DAC
+  //first measure each output voltage
+  unsigned char dac[1 << DAC_BITS];
+  for (unsigned char x = 0; x != (1 << DAC_BITS); x++) {
+	  PORTD = x << (8 - DAC_BITS);
+
+	  //let the output settle a bit - total time is still just 202 ms
+	  //TODO: put a buffer/amp between the DAC and speaker so output impedance doesn't disturb this
+	  //multiple calls because one delay(5) doesn't work properly for some reason
+	  delay(1);
+	  delay(1);
+	  delay(1);
+	  delay(1);
+	  delay(1);
+
+	  //ADC gives us 10 bits, but we only care about the DAC_BITS topmost ones
+	  PORTB = 1;
+	  dac[x] = (analogRead(DAC_IN)) >> (10 - DAC_BITS);
+	  PORTB = 0;
+  }
+
+  //next figure out which value is closest to what we want borka borka
+  for (unsigned char x = 0; x != (1 << DAC_BITS); x++) {
+	  unsigned char besty = x;
+	  unsigned char best = 0xFF;
+	  for (unsigned char y = 0; y != (1 << DAC_BITS); y++) {
+		  unsigned char diff;
+		  if (dac[y] > x) {
+			  diff = dac[y] - x;
+		  } else {
+			  diff = x - dac[y];
+		  }
+
+		  if (diff < best || (diff == best && x == y)) {
+			  best = diff;
+			  besty = y;
+		  }
+	  }
+	  lut[x] = besty << (8 - DAC_BITS);
+  }
+  PORTD=0;
+
   interrupts();             // enable all interrupts
 }
 
-void oldout() {
-	for (unsigned char x = 0; x < nkeys; x++) {
-	    digitalWrite(x + PINOFS, (osc[x] += freq[keys[x]]) >> 15);
-	}
-}
-
-void newout() {
-	unsigned char out = 0;
-	for (unsigned char x = 0; x < nkeys; x++) {
-		osc[x] += freq[keys[x]];
-		out = (out >> 1) | (osc[x] >> 8) & 0x80;
-	}
-	PORTD = out;
-}
 
 void fixout() {
 	unsigned char out = 0;
@@ -94,16 +135,28 @@ void volout() {
 			out = !(temp & 0x80) && (out & 0x80) ? 0xFF : temp;
 		}
 	}
-	PORTD = out;
+	PORTD = lut[out >> (8 - DAC_BITS)];
 }
 
+unsigned char x = 0;
 ISR(TIMER1_COMPA_vect) {
+	//PORTD = lut[(x++) >> (8 - DAC_BITS)];
+	//return;
+
   PORTB = 1;
-  //oldout();	//5 notes = 34.40 µs
-  //newout();	//5 notes = 10.56 µs
   //fixout();	//5 notes =  8.08 µs
   volout();
   PORTB = 0;
+
+  if (newkeys) {
+    PORTB = 1;
+    for (unsigned char x = 0; x < MAX_KEYS; x++) {
+      keys[x] = keys2[x];
+      vols[x] = vols2[x];
+    }
+    newkeys = 0;
+    PORTB = 0;
+  }
 }
 
 byte key = 0;
@@ -111,6 +164,21 @@ byte ctrl = 0;
 byte isvel = 0;
 byte shouldoutput = 0;
 
+void decay() {
+  for (unsigned char x = 0; x < 128; x++) {
+    if (vol[x]) {
+      vol[x]--;
+    }
+  }
+  for (unsigned char x = 0; x < MAX_KEYS; x++) {
+    if (vols[x]) {
+      vols[x]--;
+    }
+  }
+  //computeKeys();
+}
+
+byte m = 0, d = 0;
 void loop() {
   if (Serial.available() > 0) {
     byte in = Serial.read();
@@ -140,6 +208,15 @@ void loop() {
       } else {
         //pass anything that isn't "note on"
         Serial.write(in);
+      }
+    }
+  } else {
+    byte m2 = millis();
+    if (m != m2) {
+      m = m2;
+      if (d++ >= 10) {
+        decay();
+        d = 0;
       }
     }
   }
